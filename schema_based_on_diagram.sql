11f740992b007d85a96f07f06fd1837bd17fcd0c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-26 20:16:38 +0430

    Added the invoices and treatments tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,19 @@ CREATE TABLE medical_histories (
   status VARCHAR,
   CONSTRAINT fkey_patients FOREIGN KEY(patient_id) REFERENCES TO patients(id)
 );
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT NOT NULL,
+  CONSTRAINT fkey_medical_history_invoices FOREIGN KEY(medical_history_id) REFERENCES TO medical_histories(id)
+);
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  CONSTRAINT fkey_medical_history_treatments FOREIGN KEY(medical_history_id) REFERENCES TO medical_histories(id)
+);
