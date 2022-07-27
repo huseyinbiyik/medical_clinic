c0d7876d2c04c8d99bf2fe8685a38826aa10cee9
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-07-27 09:53:49 +0300

    Add bridging table for medical_histories and treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,7 @@
 CREATE TABLE patients (
   id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255) NOT NULL,
-  date_of_birth DATA NOT NULL,
+  date_of_birth DATE NOT NULL,
   PRIMARY KEY(id)
 );
 
@@ -9,37 +9,44 @@ CREATE TABLE medical_histories (
   id INT GENERATED ALWAYS AS IDENTITY,
   admitted_at TIMESTAMP,
   patient_id INTEGER NOT NULL,
-  status VARCHAR,
-  CONSTRAINT fkey_patients FOREIGN KEY(patient_id) REFERENCES TO patients(id)
-  PRIMARY KEY(id));
+  status VARCHAR NOT NULL,
+  CONSTRAINT fkey_patients FOREIGN KEY(patient_id) REFERENCES TO patients(id) PRIMARY KEY(id)
+);
 
 CREATE TABLE invoices (
   id INT GENERATED ALWAYS AS IDENTITY,
-  total_amount DECIMAL,
+  total_amount DECIMAL NOT NULL,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
   medical_history_id INT NOT NULL,
-  CONSTRAINT fkey_medical_history_invoices FOREIGN KEY(medical_history_id) REFERENCES TO medical_histories(id)
-  PRIMARY KEY(id)
+  CONSTRAINT fkey_medical_history_invoices FOREIGN KEY(medical_history_id) REFERENCES TO medical_histories(id) PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  CONSTRAINT fkey_invoice FOREIGN KEY(invoices_id) REFERENCES TO invoices(id);
+
+CONSTRAINT fkey_treatments FOREIGN KEY(treatment_id) REFERENCES TO treatments(id);
+
+PRIMARY KEY(id)
 );
 
 CREATE TABLE treatments (
   id INT GENERATED ALWAYS AS IDENTITY,
   type VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
-  CONSTRAINT fkey_medical_history_treatments FOREIGN KEY(medical_history_id) REFERENCES TO medical_histories(id)
-  PRIMARY KEY(id)
+  CONSTRAINT fkey_medical_history_treatments FOREIGN KEY(medical_history_id) REFERENCES TO medical_histories(id) PRIMARY KEY(id)
 );
 
-CREATE TABLE invoice_items (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  unit_price DECIMAL,
-  quantity INT,
-  total_price DECIMAL,
-  invoice_id INT,
+-- Bridging/joining table
+CREATE TABLE treatment_history (
+  medical_id INT,
   treatment_id INT,
-  CONSTRAINT fkey_invoice FOREIGN KEY(invoices_id) REFERENCES TO invoices(id);
-  CONSTRAINT fkey_treatments FOREIGN KEY(treatment_id) REFERENCES TO treatments(id);
-  PRIMARY KEY(id)
-);
-
+  CONSTRAINT fk_medical FOREIGN KEY (medical_id) REFERENCES medical_histories(id),
+  CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+)
